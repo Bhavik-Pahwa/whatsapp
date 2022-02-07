15d6f7a51ed2e31c37dbbfa157b28480f89803ab
Bhavik Pahwa <bhavikpahwa2007@gmail.com>
2022-02-07 13:28:19 +0530

    changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,26 @@
 import './Sidebar.css'
+import { Avatar } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className='side_header'>
+        <Avatar />
+        <div className='side_icons_right'>
+          <DonutLargeIcon />
+          <ChatIcon />
+          <MoreVertIcon />
+        </div>
+      </div>
+      <div className='side_search'>
 
       </div>
-      
+      <div className='side_chats'>
+
+      </div>
     </div>
   );
 }
